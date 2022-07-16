91eb2feecce993a40d62adace488c182b058ecb6
Kramclam <kennethramclam@gmail.com>
2022-07-16 11:23:03 -0400

    Initial upload to remote repo

---
+++ HTML_CSS_JS/script.js
@@ -0,0 +1,19 @@
+/*
+The purpose of this script is to interactively and dynamically generate a new card. 
+This card will get its information from the user input form.
+*/
+
+// TODO: Setup an event listener for the submit button
+
+// TODO: the form has to be reset once the information
+// has been submitted
+
+// TODO: build the card that will be used for populating
+// the destinations class
+
+// TODO: setup the edit button on the card
+
+// TODO: setup the remove button on card
+
+// TODO: make sure the new cards are in a flex box that is aligned
+// by rows and has a max column of 3. they need to be wrapped
