2efad6c827c08a15dc778090195cf897ae6fa586
Kramclam <kennethramclam@gmail.com>
2022-07-16 16:34:30 -0400

    updated eventhandler with new function

--- HTML_CSS_JS/script.js
+++ HTML_CSS_JS/script.js
@@ -4,6 +4,16 @@ This card will get its information from the user input form.
 */
 
 // TODO: Setup an event listener for the submit button
+document
+  .querySelector("#destination_form")
+  .addEventListener("submit", handlePrimaryBtn);
+
+function handlePrimaryBtn(event) {
+  event.preventDefault();
+
+  let { name, location, photo, description } = event.target.elements;
+  console.log(`${name.value}, ${location.value}, ${description.value}`);
+}
 
 // TODO: the form has to be reset once the information
 // has been submitted
@@ -17,3 +27,4 @@ This card will get its information from the user input form.
 
 // TODO: make sure the new cards are in a flex box that is aligned
 // by rows and has a max column of 3. they need to be wrapped
+// Listen to the form being submitted
